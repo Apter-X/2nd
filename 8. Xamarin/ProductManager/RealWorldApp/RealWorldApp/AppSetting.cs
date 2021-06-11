39bdf9904241cdab7d8978e17921f695f51452b2
Iliass Raihani <i.raihani@outlook.com>
2021-06-11 12:17:00 +0100

    porduct manager cross plateform

---
+++ 8. Xamarin/ProductManager/RealWorldApp/RealWorldApp/AppSetting.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RealWorldApp
+{
+    public class AppSetting
+    {
+        public static string ApiUrl = "https://192.168.161.1:45457/";
+    }
+}
